6d8ab92a4ddeaec7336701b93606c9e4d0df4516
John Gargalionis <johngargalionis@gmail.com>
2024-09-19 14:39:34 +0200

    Added functions to read config file

---
+++ src/config.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "config.h"
+#include "utils.h"
+
+// Function to remove surrounding quotes from a string if present
+char *remove_quotes(char *str) {
+    size_t len = strlen(str);
+    if (len > 1 && str[0] == '"' && str[len - 1] == '"') {
+        str[len - 1] = '\0';  // Remove closing quote
+        str++;  // Move past opening quote
+    }
+    return str;
+}
+
+// Function to parse config file and set connote_path
+bool parse_connote_config(const char *filename, char *connote_path, size_t connote_path_size) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "ERROR: Error opening connote config file.\n");
+        return false;
+    }
+
+    char line[MAX_CONFIG_LINE_LENGTH];
+    while (fgets(line, sizeof(line), file)) {
+        char *key = strtok(line, "=");
+        char *value = strtok(NULL, "=");
+
+        if (key && value) {
+            trim_string(key);
+            trim_string(value);
+            value = remove_quotes(value);  // Remove quotes from value
+
+            if (strcmp(key, "connote_path") == 0) {
+                strncpy(connote_path, value, connote_path_size);  // Copy string value
+            }
+        } else {
+            fprintf(stderr, "ERROR: Error reading connote config file.\n");
+            return false;
+        }
+    }
+
+    fclose(file);
+
+    return connote_path != NULL && connote_path[0] != '\0';
+}
+
+bool connote_dir(char *connote_path) {
+
+    // Expand the home directory path
+    const char *home = getenv("HOME");
+    char config_path[512] = {0};
+    snprintf(config_path, sizeof(config_path), "%s/.connote", home);
+
+    parse_connote_config(config_path, connote_path, sizeof(connote_path));
+
+    return true;
+}
---
+++ src/config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define MAX_CONFIG_LINE_LENGTH 256
+
+bool connote_dir(char *connote_path);
+
+#endif // CONFIG_H_
